0c6291a4cd411795a47bf37c1584e96df905e6ba
Pepper-Wood <Pepper-Wood@users.noreply.github.com>
2014-07-03 11:24:45 -0400

    Updated add_groups.cpp.
    notify_groups.cpp no longer needed. Also I fixed a bug in the program that make it unable to detect new entries to a_map, which would have been a problem sometime down the run. So yay that that's fixed.

--- add_groups.cpp
+++ add_groups.cpp
@@ -27,11 +27,12 @@
 #include <map>
 #include <utility>
 #include <cctype>
+#include <set>
 
 typedef std::map<std::string, std::vector<int> > adoptapedicmap;
 // within the program, the overarching one will be referred to as a_map
 typedef std::map<std::string, std::vector<int> >::iterator a_map_it;
-// iterator to be used when cycling through the map to determine 
+// iterator to be used when cycling through the map
 
 // --------------------------------------------------------------------------------
 // I wasn't able to get atoi to work for the moment, so I made my own quick
@@ -61,11 +62,23 @@ int convert_str_to_int(const std::string &input) {
 }
 
 // --------------------------------------------------------------------------------
+// returns true if a duplicate is found and false if the spotlight isn't already in a_map
+bool duplicate_found(adoptapedicmap &a_map, const std::string &spotlight) {
+	for (a_map_it iterator = a_map.begin(); iterator != a_map.end(); iterator++) {
+		// iterator->first = group name
+		if ((iterator->first) == spotlight) {
+			return true;
+		}
+	}
+	return false;
+}
+
+// --------------------------------------------------------------------------------
 // Add a new group to the adoptapedic map
 void add(adoptapedicmap &a_map, const std::string &spotlight) {
 	std::vector<int> zero_vector;
-	zero_vector.push_back(0); // this will be used as the default vector setting when groups are installed
-	a_map.insert(std::make_pair<std::string,std::vector<int> >(spotlight, zero_vector));
+	// zero_vector.push_back(0); // this will be used as the default vector setting when groups are installed
+	a_map.insert(std::pair<std::string,std::vector<int> >(spotlight, zero_vector));
 }
 
 // --------------------------------------------------------------------------------
@@ -77,10 +90,39 @@ void read_words(adoptapedicmap &a_map, std::ifstream & text_str) {
 	while (text_str >> x) {
 		if (x.size() > 1) { // if the input is a word
 			spotlight = x; // set spotlight as x
+			for (int i=0; spotlight[i]; i++) {
+				spotlight[i] = tolower(spotlight[i]); // make the word all lower case
+			}
+			// don't insert it only if the word already exists, but have it remain as spotlight in the event that categories are to be edited
+			if (!duplicate_found(a_map, spotlight)) { //duplicate not found
+				add(a_map, spotlight); // add it to a_map
+			} // don't do anything if duplicate is found
+		} else if (x.size() == 1) { // if the input is a number rather than a word
+			// convert to int and append this number to the spotlight's vector if it doesn't exist prior
+			// int num = std::atoi(x);
+			int num = convert_str_to_int(x);
+			if (std::find((a_map.find(spotlight)->second).begin(), (a_map.find(spotlight)->second).end(), num)==(a_map.find(spotlight)->second).end()) { // duplicate not found
+				a_map[spotlight].push_back(num);
+				//((a_map.find(spotlight))->second).push_back(num); // add on the new directory that the group will be a part of
+			}
+		}
+	}
+}
+
+// --------------------------------------------------------------------------------
+// Separate read_words function to account for the separate 00-NOTIFICATIONS.txt document
+void read_words_input(adoptapedicmap &a_map, std::set<std::string> &notifications, std::ifstream & text_str) {
+	std::string x; // input variable
+	std::string spotlight; // this stores the group name being processed while it's being added to categories
+	int num; // used for itoa conversion
+	while (text_str >> x) {
+		if (x.size() > 1) { // if the input is a word
+			spotlight = x; // set spotlight as x
 			for (int i=0; spotlight[i]; i++) spotlight[i] = tolower(spotlight[i]); // make the word all lower case
 			// don't insert it only if the word already exists, but have it remain as spotlight in the event that categories are to be edited
-			if (a_map.find(spotlight)!=a_map.end()) { //duplicate not found
-				add(a_map, spotlight); // add it to a_map 
+			if (!duplicate_found(a_map, spotlight)) { //duplicate not found
+				add(a_map, spotlight); // add it to a_map
+				notifications.insert(spotlight); // add it to the notifications set
 			} // don't do anything if duplicate is found
 		} else if (x.size() == 1) { // if the input is a number rather than a word
 			// convert to int and append this number to the spotlight's vector if it doesn't exist prior
@@ -133,10 +175,18 @@ void write_to_txt(adoptapedicmap &a_map, std::ofstream &out_str, const int & cat
 	}
 }
 
+// --------------------------------------------------------------------------------
+// Specific write function for the notification set.
+void write_to_notification_list(std::set<std::string> &notifications, std::ofstream &out_str) {
+	for (std::set<std::string>::iterator iterator = notifications.begin(); iterator != notifications.end(); iterator++) {
+		out_str << ":icon" << *iterator << ":" << std::endl;
+	}
+}
+
 // ================================================================================
 int main(int argc, char* argv[]) {
 	adoptapedicmap a_map;
-		
+	
 	// ALL.txt - file that stores the information of all the groups already stored
 	// open ALL.txt and read into a_map
 	std::ifstream all_str("00-ALL.txt");
@@ -146,6 +196,7 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	read_words(a_map, all_str);
+	std::set<std::string> notifications; // create a set specifically to check for new added groups.
 	
 	// INPUT.txt - file that stores the information for new groups to be added
 	// open INPUT.txt and read it into a_map as well, thereby combinging the two
@@ -154,7 +205,7 @@ int main(int argc, char* argv[]) {
 		std::cerr << "Could not open 00-INPUT.txt to read\n";
 		return 1;
 	}
-	read_words(a_map, input_str);
+	read_words_input(a_map, notifications, input_str);
 	
 	// open up ALL.txt so that it can be rewritten with the combined text
 	std::ofstream all_str_rewrite("00-ALL.txt");
@@ -211,5 +262,12 @@ int main(int argc, char* argv[]) {
 		write_to_txt(a_map, agencies_txt_str, 7);
 	} else { return 0; }
 	
+	// open up NOTIFICATIONS.txt so that it can be rewritten with the combined text
+	std::ofstream n_str("00-NOTIFICATIONS.txt");
+	if (n_str.is_open()) {
+		write_to_notification_list(notifications, n_str);
+	} else { return 0; }
+	
 	std::cout << "Finished adding groups." << std::endl;
+	std::cout << "There are currently " << (a_map.size()) << " groups in the directory." << std::endl;
 }
