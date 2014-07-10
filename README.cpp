9dd72733f6599f03d3b32a006c53b082933509f3
Pepper-Wood <Pepper-Wood@users.noreply.github.com>
2014-07-10 16:03:53 -0400

    Update README.cpp
    Accounted for categories by number of watchers

--- README.cpp
+++ README.cpp
@@ -2,26 +2,29 @@
 
 // Complete directory of files that I have within the Adoptapedia folder on my desktop:
 // Numbers have been added in front of the text documents so as to "alphabetize" them in a logical order. Those with 000 prefaces are files that are actively edited within the program and are the core databases
-00-ALL.txt					// complete collection of all the groups already stored. This is the main file that is accessed by everything
-00-INPUT.txt				// list of groups to add as well as their corresponding categories
+00-ALL.txt		// complete collection of all the groups already stored. This is the main file that is accessed by everything
+00-DELETE.txt                               // contains a list of group names that will be removed from the directory because either the name was misspelled during processing or the group is currenlty deactivated
+00-INPUT.txt		// list of groups to add as well as their corresponding categories
 00-NOTIFICATIONS.txt		// stores htmled versions of the groups written in INPUT.txt
 
 // Single integer prefaces are for the separate categories as labelled by number
-0-all_text.txt				// complete list of all the adoptable groups stored
-1-all_accepted.txt			// adoptable groups that accept all kinds of adopts
-2-species.txt				// adopt groups that accept only species-related adopts
-3-fandom.txt				// adopt groups made for specific fandoms
-4-payment.txt				// adopt groups that only accept a certain form of payment
-5-quality.txt				// adopt groups that only accept adopts deemed of a certain standard of quality
-6-bases.txt					// groups that offer bases to use for adopts
-7-agencies.txt				// RP-based adoptable agencies where you can purchase an adopt to use within the world
+0-all_text.txt		// complete list of all the adoptable groups stored
+1-all_accepted.txt		// adoptable groups that accept all kinds of adopts
+2-species.txt		// adopt groups that accept only species-related adopts
+3-fandom.txt		// adopt groups made for specific fandoms
+4-payment.txt		// adopt groups that only accept a certain form of payment
+5-quality.txt		// adopt groups that only accept adopts deemed of a certain standard of quality
+6-bases.txt		// groups that offer bases to use for adopts
+7-agencies.txt		// RP-based adoptable agencies where you can purchase an adopt to use within the world
+8-1_100.txt                                 // groups with between 1 to 100 watchers
+9-100_500.txt                               // groups with between 100 to 500 watchers
+10-500_1000.txt                             // groups with between 500 to 1000 watchers
+11-1000_2000.txt                            // groups with between 1000 and 2000 watchers
+12-2000_3000.txt                            // groups with between 2000 and 3000 watchers
+13-3000+.txt                                // groups with more than 3000 watchers
 
-// The following are the .cpp files used to modify the .txt files
-add_groups.cpp				// adds groups within the INPUT.txt file to the group directory and formats .txt files accordingly
-add_groups.exe
-add_groups.exe.stackdump
-                      // These two files used to be in the directory, but I have since merged the code to add_groups
-//notify_new_groups.cpp		// transforms the groups listed in INPUT.txt to NOTIFICATIONS.txt to make sending notifications easier
-//notify_new_groups.exe
+// the core program files. 
+main.cpp
+main.exe
 
-README.cpp					// the file that you are currently reading ;)
+README.cpp            	// the file that you are currently reading ;)
