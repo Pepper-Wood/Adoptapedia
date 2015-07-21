59612fba06f509fe2d33a3d46331bf8f36b22715
Pepper-Wood <dipippo.k@gmail.com>
2015-07-21 18:02:59 -0400

    readWebpage completed
    Successfully returns an integer of the number of watchers a group has.
    Also can read in a text file (currently practicing with testdata.txt)
    and return values.

--- adoptapedia_readWebpage.py
+++ adoptapedia_readWebpage.py
@@ -4,39 +4,50 @@ Basic program to take a webpage and return the number of watchers a group has
 
 import urllib2
 from HTMLParser import HTMLParser
+from string import whitespace
 
 # -----------------------------------------------------------
-def hasNumbers(inputString):
+def hasNumbers(inputString): #{
 	return any(char.isdigit() for char in inputString)
+#}
 
 # -----------------------------------------------------------
-def return_num_of_watchers(group_name):
+def return_num_of_watchers(group_name): #{
 	url_name = "http://" + group_name + ".deviantart.com"
 	req = urllib2.Request(url_name)
 	response = urllib2.urlopen(req)
 	page_source = response.read()
 	
 	watch_list = []
-	# -----------------------------------------------------------
+	
+	# ---------------------------------------------------
 	# create a subclass and override the handler methods
 	class MyHTMLParser(HTMLParser):
 		def handle_data(self, data):
 			if data.find("atchers") != -1:
 				watch_list.append(data)
-
+	# ---------------------------------------------------
 	# instantiate the parser and fed it some HTML
 	parser = MyHTMLParser()
 	parser.feed(page_source)
 	
+	watch_integer = ''
 	for i in range(0, len(watch_list)):
 		watch_list[i] = watch_list[i].replace(' Watchers','')
 		if hasNumbers(watch_list[i]):
 			watch_integer = watch_list[i]
-		#print "ITEM " + str(i) + ':  "' + watch_list[i] + '"'
 
-	print 'BEFORE:' + str(watch_integer)
-	final_int = int(watch_integer.replace(',',''))
-	print 'AFTER: ' + str(final_int)
-	
-	
-return_num_of_watchers("adoptapedia")
+	if ',' in watch_integer:
+		final_int = int(watch_integer.replace(',',''))
+	else:
+		final_int = int(watch_integer)
+	return final_int
+#}
+
+# ===========================================================
+text_file = open("testdata.txt", "r")
+groupNames = text_file.readlines()
+
+for i in range(0, len(groupNames)):
+	groupNames[i] = groupNames[i].translate(None, whitespace)
+	print str(i) + ':   ' + groupNames[i] + '   ' + str(return_num_of_watchers(groupNames[i]))
